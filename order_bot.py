91c771e9f580ba6aef3abe239a4ab4f35df19064
iron-66 <ironman4000000@gmail.com>
2025-03-08 02:50:48 +0500

    Edit UserAgent argument

--- order_bot.py
+++ order_bot.py
@@ -243,8 +243,8 @@ def process_order(product_urls: list[str]):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                                "(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+    #chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+    #                            "(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
     driver = webdriver.Chrome(options=chrome_options)
 
     try:
