28463c3b0db8338a3f504c6cc6e4061ee0427a35
iron-66 <ironman4000000@gmail.com>
2025-03-08 03:46:48 +0500

    Edit UserAgent arguments

--- test_selenium.py
+++ test_selenium.py
@@ -7,8 +7,6 @@ options.add_argument("--log-level=3")
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--user-data-dir=/tmp/chrome-profile")
-options.add_argument("--profile-directory=Default")
 
 #options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
 #                                "(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
