6ff981d22e26949e115291526b56a846e50d3c6b
iron-66 <ironman4000000@gmail.com>
2025-03-08 02:45:07 +0500

    Edit test file

--- test_selenium.py
+++ test_selenium.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--log-level=3")
 driver = webdriver.Chrome(options=options)
 
 start = time.time()
