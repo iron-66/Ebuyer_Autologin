9aba5d94afb068037fb6e03f006a9654168c1b4f
iron-66 <ironman4000000@gmail.com>
2025-03-08 02:46:12 +0500

    Edit test file

--- test_selenium.py
+++ test_selenium.py
@@ -7,6 +7,8 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--log-level=3")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                                "(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
 driver = webdriver.Chrome(options=options)
 
 start = time.time()
