1315bc67ab8727d6b01a6f952c0ac87f78d8f36c
iron-66 <ironman4000000@gmail.com>
2025-03-08 01:26:23 +0500

    Added Selenium launch test

---
+++ test_selenium.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Без интерфейса
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+try:
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.google.com")
+    print("Selenium запущен успешно!")
+    driver.quit()
+except Exception as e:
+    print(f"Ошибка запуска Selenium: {e}")
