d9973155cfc39420897c46b32b0ffb78429cebb2
iron-66 <ironman4000000@gmail.com>
2025-03-08 03:48:27 +0500

    Edit UserAgent arguments

--- test_selenium.py
+++ test_selenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--log-level=3")
-options.add_argument("--headless=new")
+#options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
