21265ab693a9a2b2e5ff0038f67b18a2f3fd5485
iron-66 <ironman4000000@gmail.com>
2025-03-08 02:43:38 +0500

    Edit test file

--- test_selenium.py
+++ test_selenium.py
@@ -1,15 +1,15 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-# chrome_options.add_argument("--headless")  # Без интерфейса
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 
-try:
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://www.sainsburys.co.uk/gol-ui/groceries")
-    print("Selenium запущен успешно!")
-    driver.quit()
-except Exception as e:
-    print(f"Ошибка запуска Selenium: {e}")
+start = time.time()
+driver.get("https://www.sainsburys.co.uk/gol-ui/groceries")
+end = time.time()
+
+print(f"Page load time: {end - start:.2f} seconds")
+driver.quit()
+
