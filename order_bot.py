d60aabf6a78d4d4ebbcee42e7c7ea073a9c2271b
iron-66 <ironman4000000@gmail.com>
2025-03-08 01:53:44 +0500

    Commented headless argument

--- order_bot.py
+++ order_bot.py
@@ -236,7 +236,7 @@ def select_delivery_slot(driver):
 def process_order(product_urls: list[str]):
     chrome_options = Options()
     chrome_options.add_argument("--log-level=3")
-    chrome_options.add_argument("--headless")
+    #chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
