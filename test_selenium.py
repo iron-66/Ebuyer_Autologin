bb192cd58b61a65018297b94e2c7e4cdbcb592eb
iron-66 <ironman4000000@gmail.com>
2025-03-08 03:50:10 +0500

    Edit UserAgent arguments

--- test_selenium.py
+++ test_selenium.py
@@ -4,16 +4,16 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--log-level=3")
-#options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 #options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
 #                                "(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
 driver = webdriver.Chrome(options=options)
-# driver.execute_cdp_cmd("Network.setUserAgentOverride", {
-#     "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36"
-# })
+driver.execute_cdp_cmd("Network.setUserAgentOverride", {
+    "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36"
+})
 
 start = time.time()
 driver.get("https://www.sainsburys.co.uk/gol-ui/groceries")
