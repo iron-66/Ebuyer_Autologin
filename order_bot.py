1aaec9bb9351fc1226006fc3c3ec2168b3a96116
iron-66 <ironman4000000@gmail.com>
2025-03-08 00:57:58 +0500

    Added no-sandbox argument

--- order_bot.py
+++ order_bot.py
@@ -237,6 +237,7 @@ def process_order(product_urls: list[str]):
     chrome_options = Options()
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                                 "(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
